f5da718b4c8f52d1adbdc08774c92945a7e5bcf4
kissyossy <tsuipayato@gmail.com>
2021-01-17 22:08:00 +0900

    トラッキング

--- modering/mode/modering2.php
+++ modering/mode/modering2.php
@@ -23,7 +23,7 @@
 <div id="stage"></div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/99/three.min.js"></script>
-<script src="https://threejs.org/examples/jsm/controls/TrackballControls.js"></script>
+<script src="../js_r79/TrackballControls.js"></script>
 <script>
     var scene;
     var box;
@@ -33,6 +33,18 @@
     var width = 800;
     var height = 600;
     var projector = new THREE.Projector();
+    // この平面に対してオブジェクトを平行に動かす
+			var plane = new THREE.Plane();
+
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+var offset = new THREE.Vector3();
+var intersection = new THREE.Vector3();
+
+// マウスオーバーしているオブジェクト
+var mouseoveredObj;
+// ドラッグしているオブジェクト
+var draggedObj;
 (function init(){
     'use strict';
 
@@ -100,6 +112,8 @@ var button1 = document.getElementById("button1");
     }
     render();
 
+    var controls = new THREE.TrackballControls( camera, renderer.domElement );
+
     //軸
     var material1 = new THREE.LineBasicMaterial( { linewidth: 30, color:"rgb(0,0,0)" } );
     var geometry1 = new THREE.Geometry();
@@ -147,11 +161,83 @@ window.onmousedown = function (ev){
  
     }
    }; 
-   controls = new TrackballControls(camera, renderer.domElement);
-   function animate() {
-      requestAnimationFrame(animate);
-      controls.update();
-    }
+
+   renderer.domElement.addEventListener( 'mousedown', onDocumentMouseDown, false );
+			renderer.domElement.addEventListener( 'mousemove', onDocumentMouseMove, false );
+			renderer.domElement.addEventListener( 'mouseup', onDocumentMouseUp, false );
+
+			function onDocumentMouseDown( event ) {
+				event.preventDefault();
+
+				raycaster.setFromCamera( mouse, camera );
+				var intersects = raycaster.intersectObjects( objects );
+
+				if ( intersects.length > 0 ) {
+					// マウスドラッグしている間はTrackballControlsを無効にする
+					controls.enabled = false;
+
+					draggedObj = intersects[ 0 ].object;
+
+					// rayとplaneの交点を求めてintersectionに設定
+					if ( raycaster.ray.intersectPlane( plane, intersection ) ) {
+						// ドラッグ中のオブジェクトとplaneの距離
+						offset.copy( intersection ).sub( draggedObj.position );
+					}
+				}
+			}
+
+			function onDocumentMouseMove( event ) {
+				event.preventDefault();
+
+				mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+				mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+				raycaster.setFromCamera( mouse, camera );
+
+				if ( draggedObj ) {
+					// オブジェクトをドラッグして移動させているとき
+
+					// rayとplaneの交点をintersectionに設定
+					if ( raycaster.ray.intersectPlane( plane, intersection ) ) {
+						// オブジェクトをplaneに対して平行に移動させる
+						draggedObj.position.copy( intersection.sub( offset ) );
+					}
+				} else {
+					// オブジェクトをドラッグしないでマウスを動かしている場合
+					var intersects = raycaster.intersectObjects( targetList );
+
+					if ( intersects.length > 0 ) {
+						if ( mouseoveredObj != intersects[ 0 ].targetList ) {
+							// マウスオーバー中のオブジェクトを入れ替え
+							mouseoveredObj = intersects[ 0 ].targetList;
+
+							// plane.normalにカメラの方向ベクトルを設定
+							// 平面の角度をカメラの向きに対して垂直に維持する
+							camera.getWorldDirection( plane.normal );
+						}
+					} else {
+						mouseoveredObj = null;
+					}
+				}
+			}
+
+			function onDocumentMouseUp( event ) {
+				event.preventDefault();
+
+				controls.enabled = true;
+
+				if ( mouseoveredObj ) {
+					draggedObj = null;
+				}
+			}
+
+			animate();
+			function animate() {
+				requestAnimationFrame( animate );
+				controls.update();
+				renderer.render( scene, camera );
+			}
+
     
 })();
 

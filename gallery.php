864ea199906936d26dcce0fda97c06acd85e446f
Yoshito Hirasawa <melontyahan@gmail.com>
2021-01-19 19:32:43 +0900

    ギャラリーです。

---
+++ gallery.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>ギャラリー<h1>
+<h3>作成した画像を選択してください。<h3>
+
+<script>
+
+function onFileSelected(input) {
+
+    var file = input.files[0];
+
+    var reader = new FileReader();
+
+    reader.onload = onFileLoaded;
+
+    reader.readAsDataURL(file);
+
+}
+
+function onFileLoaded(e) {
+
+    var src_data = e.target.result;
+
+    var img = new Image();
+
+    img.onload = onImageSetted;
+    img.src = src_data;
+
+}
+
+function onImageSetted(e) {
+
+    var data = createImageData(e.target);
+
+    document.getElementById('test_canvas').getContext('2d').putImageData(data, 0, 0);
+
+}
+
+function createImageData(img) {
+
+    var cv = document.createElement('canvas');
+
+    cv.width = img.naturalWidth;
+    cv.height = img.naturalHeight;
+
+    var ct = cv.getContext('2d');
+
+    ct.drawImage(img, 0, 0);
+
+    var data = ct.getImageData(0, 0, cv.width, cv.height);
+
+    return data;
+
+}
+
+</script>
+
+<p>
+<input type="file" onchange="onFileSelected(this)">
+</p>
+
+<p>
+<canvas id="test_canvas" width=400 height=400 style="border: 1px solid;"></canvas>
+</p>
+
+</body>
+</html>

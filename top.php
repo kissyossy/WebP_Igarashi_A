9b6bca0ace8201cb296e7a2afa4a2980aba311fa
kissyossy <tsuipayato@gmail.com>
2021-01-20 10:28:01 +0900

    no message

---
+++ top.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Creation</title>
+        <link rel="stylesheet" href="top.css">
+    </head>
+    <body>
+        <h1>CREATION</h1>
+        
+        <p>
+        <a  class="model" href="modering.php">
+            モデリングに移動
+        </a>
+        </p>
+        <div class = "model-hukidashi">
+            新しいモデリングをつくる！！
+        </div>
+
+        <p>
+        <a  class="gallery" href="gallery.php">
+            ギャラリーに移動
+        </a>
+        </p>
+
+        <div class = "gallery-hukidashi">
+            作った作品を見る！！！！
+        </div>
+
+    </body>
+ </html>

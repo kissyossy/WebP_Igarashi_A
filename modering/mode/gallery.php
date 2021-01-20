a8877f666c125434a19a8c1a783c7ed04f5a5413
Yoshito Hirasawa <melontyahan@gmail.com>
2021-01-20 14:00:38 +0900

    ギャラリー再変更

--- modering/mode/gallery.php
+++ modering/mode/gallery.php
@@ -1,32 +1,71 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <link rel = "stylesheet" href = "style.css">
 
 <h1>『ギャラリー』<h1>
 <h3>作成した画像を選択してください。<h3>
 
-<input type="file" id="selfile"><br>
-
-<div id="bg"></div>
-
-<style type="text/css">
-
-div#bg{ width:960px;  height:540px; border:5px solid black; overflow:auto }
-</style>
-
 <script>
-var obj1 = document.getElementById("selfile");
 
-obj1.addEventListener("change", function(evt){
-  var file = evt.target.files;
-  var reader = new FileReader();
-  
-  reader.readAsDataURL(file[0]);
-  
-  reader.onload = function(){
-    var dataUrl = reader.result;
+function onFileSelected(input) {
 
-    document.getElementById("bg").innerHTML = "<img src='" + dataUrl + "'>";
-    document.getElementById("dturl").value = dataUrl;
-  }
-},false);
+    var file = input.files[0];
+
+    var reader = new FileReader();
+
+    reader.onload = onFileLoaded;
+
+    reader.readAsDataURL(file);
+
+}
+
+function onFileLoaded(e) {
+
+    var src_data = e.target.result;
+
+    var img = new Image();
+
+    img.onload = onImageSetted;
+    img.src = src_data;
+
+}
+
+function onImageSetted(e) {
+
+    var data = createImageData(e.target);
+
+    document.getElementById('test_canvas').getContext('2d').putImageData(data, 0, 0);
+
+}
+
+function createImageData(img) {
+
+    var cv = document.createElement('canvas');
+
+    cv.width = img.naturalWidth;
+    cv.height = img.naturalHeight;
+
+    var ct = cv.getContext('2d');
+
+    ct.drawImage(img, 0, 0);
+
+    var data = ct.getImageData(0, 0, cv.width, cv.height);
+
+    return data;
+
+}
 
 </script>
+
+<p>
+<input type="file" onchange="onFileSelected(this)">
+</p>
+
+<p>
+<canvas id="test_canvas" width=600 height=400 style="border: 1px solid;"></canvas>
+</p>
+
+</body>
+</html>

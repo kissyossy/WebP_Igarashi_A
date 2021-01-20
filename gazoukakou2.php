9f81389ba63db44b005217a73d754ffe9a6fb038
adachi <adachi1679@gmail.com>
2021-01-20 09:07:42 +0900

    できた!!!

---
+++ gazoukakou2.php
@@ -0,0 +1,31 @@
+<?php
+$format = '%s.jpg';
+$gazou = $_FILES['img']['name'];
+$num = mb_strlen($gazou); 
+$word = mb_substr($gazou,0,$num-4);
+$name = sprintf($format,$word);
+print $name . '<br>';
+?>
+
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+	<meta charset='UTF-8'>
+	<title> 画像加工 </title>
+</head>
+
+<body>
+	<form action="gazoukakou3.php" method="GET">
+		加工内容を選択してください<br>
+		<input type="checkbox" name="gd[]" value="nega"> ネガ(色反転) <br>
+		<input type="checkbox" name="gd[]" value="gray"> グレースケール(無彩色) <br>
+		<input type="checkbox" name="gd[]" value="gauss"> ぼかし <br>
+		輝度(明度)<input type="text" name="kido">※1~255の範囲でないと動きません<br>
+		<input type="hidden" name="name" value="<?php print $name; ?>">
+		<br>
+		<input type="submit" value="送信">
+	</form>
+</body>
+
+</html>
---
+++ gazoukakou3.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_GET["name"];
+$img = ImageCreateFromJPEG($name);
+//$img = ImageCreateFromJPEG(sprintf('inko.jpg'));
+
+if(in_array('nega', $_GET['gd'])){
+  ImageFilter($img, IMG_FILTER_NEGATE);
+}
+if(in_array('gray', $_GET['gd'])){
+  ImageFilter($img, IMG_FILTER_GRAYSCALE);
+}
+if(in_array('gauss', $_GET['gd'])){
+  $matrix = array(array(1, 1, 1), array(1, 1, 1), array(1, 1, 1));
+  imageconvolution($img, $matrix, 9, 0);
+}
+
+$kido = $_GET["kido"];
+ImageFilter($img, IMG_FILTER_BRIGHTNESS, $kido);
+
+//move_uploaded_file($img, ../);
+
+header('Content-Type: image/jpeg');
+ImageJPEG($img, './output.jpg');
+header("location: gazoukakouEnd.php");
+?>

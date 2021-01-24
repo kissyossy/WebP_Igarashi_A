ea0c2dae235f170fe411d57a371808c667b0c399
adachi <adachi1679@gmail.com>
2021-01-25 02:53:15 +0900

    これだけほんとに修正版

--- 最終版(gazoukakouEnd.phpは消去)/gazoukakou2.php
+++ 最終版(gazoukakouEnd.phpは消去)/gazoukakou2.php
@@ -1,9 +1,5 @@
 <?php
-$format = '%s.png';
-$gazou = $_FILES['img']['name'];
-$num = mb_strlen($gazou); 
-$word = mb_substr($gazou,0,$num-4);
-$name = sprintf($format,$word);
+$name = $_FILES['img']['name'];
 ?>
 
 <!DOCTYPE html>
